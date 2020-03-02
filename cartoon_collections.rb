05ab655b58a61bc3120a681ec1e6562a9a9f33ad
Edward Horsey <edwardhorsey@me.com>
2020-03-02 13:14:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index {
+    |item, index| puts "#{index+1}. #{item}"
+}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+list.map {
+    |item| 
+ "#{item.capitalize}!"
+}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(list)
+    index=0
+    while index<list.count do
+        if list[index].length > 4
+        return true    
+        else
+        index += 1
+        end
+    end
+return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(list) # code an argument here
+# the array below is here to help
+cheese_types = ["cheddar", "gouda", "camembert"]    
+index = 0
+while index<cheese_types.count do
+    if list.include?(cheese_types[index])
+        return cheese_types[index]
+    end
+    index += 1
 end
+nil
+end
+
